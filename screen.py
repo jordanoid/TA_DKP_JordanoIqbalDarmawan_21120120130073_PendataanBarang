7e6841f6198a69a3932b316f03b2769da2eff58d
jordanoid <jordanoid2@gmail.com>
2021-05-26 18:12:23 +0700

    added functional show list screen

--- screen.py
+++ screen.py
@@ -24,7 +24,7 @@ def homescreen():
     stg_label.place(relx = 0.69, rely = 0.65)
 
     list_image = PhotoImage(file = "images/list_button.png")
-    list_button = Button(frame, image = list_image, borderwidth = 5, command = testButton, bg = "#3bb9eb")
+    list_button = Button(frame, image = list_image, borderwidth = 5, command = listscreen, bg = "#3bb9eb")
     list_button.place(relx = 0.09, rely = 0.3)
     list_label = Label(frame, text = "Daftar Barang", bg = "#3bb9eb")
     list_label.place(relx = 0.085, rely = 0.65)
@@ -37,6 +37,7 @@ def homescreen():
 
 def setscreen():
     global screen2
+    screen1.destroy()
     screen2 = Toplevel()
     screen2.geometry("300x250")
     frame = Frame(screen2, bg = "#3bb9eb", bd = 5 )
@@ -58,7 +59,28 @@ def setscreen():
         newPass = pw.get()
         changeCredential(newUname, newPass)
         screen2.destroy()
-    ok_button = Button(frame, text = "Ok", bg = "#3bb9eb", command = ok)
+        homescreen()
+    ok_button = Button(frame, text = "OK", bg = "#3bb9eb", command = ok)
     ok_button.pack()
 
-    
+def listscreen():
+    def ok():
+        screen3.destroy()
+        homescreen()
+    global data
+    screen1.destroy()
+    data = {"bangku" : 10, "meja" : 10, "papan tulis" : 2, "spidol" : 3}
+    screen3 = Toplevel()
+    frame = Frame(screen3, bg = "#3bb9eb", bd = 5 )
+    frame.place(relx = 0.1, rely = 0.1, relwidth = 0.8, relheight = 0.8)
+    label1 = Label(frame, text = "Nama barang", bg = "#3bb9eb")
+    label1.grid(column = 0, row = 0)
+    label2 = Label(frame, text = "Jumlah", bg = "#3bb9eb")
+    label2.grid(column = 2, row = 0)
+    counter = 1
+    for i in data:
+        Label(frame, text = i, bg = "#3bb9eb").grid(column = 0, row = counter)
+        Label(frame, text = data[i], bg = "#3bb9eb").grid(column = 2, row = counter)
+        counter += 1
+    ok_button = Button(screen3, text = "Back", bg = "#3bb9eb", command = ok)
+    ok_button.place(relx = 0.7, rely = 0.7)
